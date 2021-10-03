71df79b9f157b42caa08c53384d9cfa7a2ee4659
ym2629 <dydals2629@naver.com>
2021-10-03 20:24:04 +0900

    test

--- src/URL.java
+++ src/URL.java
@@ -1,4 +1,44 @@
+import java.util.Iterator;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class URL {
-
+	
+	String base_url;
+	
+	URL(String url){
+		System.out.println("asdf");
+		this.base_url = url;
+	}
+	
+	void Connet_URL(String url) { //connet url
+		Document doc = null;   
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	Elements set_Element(Document doc, String cla) {
+		return doc.select(cla);  
+	}
+	
+	void Element_print_text(Elements element) {
+		Iterator<Element> ie1 = element.iterator();
+		while(ie1.hasNext()) {
+			System.out.println(ie1.next().text());
+		}
+	}
+	
+	void Element_print_attr(Elements element, String cla) {
+		Iterator<Element> ie1 = element.iterator();
+		while(ie1.hasNext()) {
+			System.out.println(ie1.next().attr(cla));
+		}
+	}
+	
 }
