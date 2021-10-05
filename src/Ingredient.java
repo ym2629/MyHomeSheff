9404e200b9e016fa4017e1477d5ae345c45fe455
ym2629 <dydals2629@naver.com>
2021-10-05 12:44:45 +0900

    test crawling

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 
 public class Ingredient {
-	ArrayList[] ;
+	public static void main(String[] arsg) throws Exception{
+		ArrayList[] ingrdient;
+		URL url = new URL(); //나무위키
+		String aurl = "https://www.10000recipe.com/recipe/list.html";
+		Document doc = url.connectURL(aurl);
+	
+		Elements ele = doc.select(".rcp_cate.st3");
+		ele = ele.select(".cate_list > a");
+		url.elementPrintText(ele,18);
+		System.out.println("===================");
+		ArrayList<String> list = url.elementsList(ele,18);
+		for(String str : list) {
+			System.out.println(str);
+		}
+
+		
+		System.out.println("=======================================================");
+		
+		String url_2 = aurl+"?q="+URLEncoder.encode("밑반찬","UTF-8");
+		Document doc1 = Jsoup.connect(url_2).get();
+		Elements ele1 = doc1.select(".tag_cont > li > a");
+		url.elementPrintText(ele1,999);
+	}
+	
 }
--- src/URL.java
+++ src/URL.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -6,16 +8,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class URL {
-	
 	String baseURL;
+	
 	URL(){
-		this.baseURL = https://www.10000recipe.com/index.html;
+		this.baseURL = "https://www.10000recipe.com/index.html";
 	}
 	URL(String url){
 		this.baseURL = url;
 	}
-	
-	Document connectURL(String url) { //connet url
+	Document connectURL(String url){ //connet url
 		Document doc = null;   
 		try {
 			doc = Jsoup.connect(url).get();
@@ -24,19 +25,36 @@ public class URL {
 		}
 		return doc;
 	}
-	
 	Elements getElement(Document doc, String selector) {
-		return doc.select(selector);  
+		Elements ele = null;
+		try {
+			return doc.select(selector); 
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return ele;
 	}
-	
-	void elementPrintText(Elements element) {
+	void elementPrintText(Elements element, int number) throws Exception{
 		Iterator<Element> ie1 = element.iterator();
+		int i = 0;
+		
 		while(ie1.hasNext()) {
+			if(i == number) {
+				break;
+			}
 			System.out.println(ie1.next().text());
+			i++;
 		}
 	}
-	
-	void elementPrintAttr(Elements element, String attrName) {
+	ArrayList elementsList(Elements element, int number) {
+		ArrayList list = new ArrayList();
+		Iterator<Element> ie1 = element.iterator();
+		for(int i=0;i<number;i++) {
+			list.add(ie1.next().text()); 
+		}
+		return list;	
+	}
+	void elementPrintAttr(Elements element, String attrName) throws Exception {
 		Iterator<Element> ie1 = element.iterator();
 		while(ie1.hasNext()) {
 			System.out.println(ie1.next().attr(attrName));
